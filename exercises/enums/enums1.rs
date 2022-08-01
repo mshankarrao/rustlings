6ac4673a482ef16b37bdfca987efc88d74265e01
Shankar Rao Mata <mshankarrao1970@gmail.com>
2022-08-01 01:07:27 -0400

    completed enums and strings

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -1,13 +1,16 @@
 // enums1.rs
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 enum Message {
     // TODO: define a few types of messages as used below
+    ChangeColor,
+    Echo,
+    Move,
+    Quit
 }
-
+ 
 fn main() {
     println!("{:?}", Message::Quit);
     println!("{:?}", Message::Echo);
