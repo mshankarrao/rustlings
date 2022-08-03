59e0ea24b3939779dc6859c3dd316cae6b1cd9df
Shankar Rao Mata <mshankarrao1970@gmail.com>
2022-08-03 01:06:30 -0400

    completed errors, varibales, structs, hashmap,primitive_types,quiz1, if

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -28,7 +26,6 @@ struct Team {
 fn build_scores_table(results: String) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
-
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
         let team_1_name = v[0].to_string();
@@ -40,6 +37,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        // scores.insert(team_1_name,Team{name: team_1_name, goals_scored: team_1_score, goals_conceded: team_2_score });
+        // scores.insert(team_2_name,Team{name: team_2_name, goals_scored: team_2_score, goals_conceded: team_1_score });
+        scores
+            .entry(team_1_name)
+            .and_modify(|e| {
+                e.goals_scored += team_1_score;
+                e.goals_conceded += team_2_score;
+            })
+            .or_insert_with_key(|name| Team {
+                name: name.to_string(),
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
     }
     scores
 }
